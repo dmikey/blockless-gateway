78e2173b800311dc1ce514654b6006bc4d63cc2c
Udit <udit@eskaytech.com>
2023-07-21 03:06:42 +0530

    Add selective cache for site's static assets

--- src/services/headNodeCache.ts
+++ src/services/headNodeCache.ts
@@ -4,6 +4,30 @@ import { IHeadNodeResponse } from '../interfaces/headNode'
 import { jsonToBase64 } from '../utils/buffer'
 
 const nodeCache = new NodeCache()
+const mimeTypesForCaching = [
+	'image/jpeg',
+	'image/png',
+	'image/gif',
+	'image/webp',
+	'image/svg+xml',
+	'text/css',
+	'application/javascript',
+	'application/font-woff',
+	'application/font-woff2',
+	'application/vnd.ms-fontobject',
+	'application/x-font-ttf',
+	'audio/mpeg',
+	'video/mp4',
+	'application/pdf',
+	'application/json',
+	'application/xml',
+	'application/octet-stream',
+	'text/plain',
+	'application/zip',
+	'application/x-rar-compressed',
+	'application/x-tar',
+	'application/x-gzip'
+]
 
 /**
  * Store head node response in cache
@@ -16,8 +40,13 @@ export async function storeCache(
 	payload: IHeadNodePayload,
 	data: IHeadNodeResponse
 ): Promise<IHeadNodeResponse> {
-	const cacheId = jsonToBase64(payload)
-	nodeCache.set(cacheId, data)
+	const result = data.result
+	const resultMimeType = extractMimeType(result || '')
+
+	if (resultMimeType && mimeTypesForCaching.indexOf(resultMimeType) !== -1) {
+		const cacheId = jsonToBase64(payload)
+		nodeCache.set(cacheId, data)
+	}
 
 	return data
 }
@@ -32,4 +61,22 @@ export async function fetchCache(
 ): Promise<IHeadNodeResponse | undefined> {
 	const cacheId = jsonToBase64(payload)
 	return await nodeCache.get(cacheId)
+}
+
+/**
+ * Utility function extract mimetype from Head node's response
+ *
+ * @param dataString
+ * @returns
+ */
+function extractMimeType(dataString: string): string | null {
+	const regex = /^data:(.*?);base64,(.*)$/
+	const match = dataString.match(regex)
+
+	if (match && match.length === 3) {
+		const mimeType = match[1]
+		return mimeType
+	} else {
+		return null
+	}
 }
