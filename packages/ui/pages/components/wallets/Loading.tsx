8ec523a66a64375deaf8fc82ab852d578320a1f1
Udit <udit@eskaytech.com>
2023-08-03 13:56:04 +0530

    Setup login page with metamask auth option

---
+++ packages/ui/pages/components/wallets/Loading.tsx
@@ -0,0 +1,38 @@
+import { motion } from 'framer-motion'
+
+import { Text, VStack } from '@blocklessnetwork/ui-components'
+
+interface LoadingProps {
+	walletType: 'metamask' | 'keplr' | 'martianWallet'
+	reloading: boolean // state when already have the wallet connected, but need to authorize with the api
+	redirecting: boolean
+}
+
+export default function WalletLoading({ walletType, reloading, redirecting }: LoadingProps) {
+	if (reloading || redirecting) {
+		return (
+			<VStack spacing={5}>
+				<Text color="primary.gray.1" fontSize="14px" fontWeight={400}>
+					{redirecting ? 'Redirecting' : 'Authenticating'}
+				</Text>
+				<div className="dot-collision" />
+			</VStack>
+		)
+	} else {
+		return (
+			<VStack spacing={5}>
+				<motion.img
+					animate={{
+						scale: [1, 1.2, 1]
+					}}
+					transition={{ repeat: Infinity }}
+					src={`${walletType}.svg`}
+					alt={walletType}
+				/>
+				<Text color="primary.gray.1" fontSize="16px" fontWeight={400}>
+					Continue in <span style={{ textTransform: 'capitalize' }}>{walletType}</span>
+				</Text>
+			</VStack>
+		)
+	}
+}
--- packages/ui/pages/root/404.tsx
+++ packages/ui/pages/root/404.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import Base from '../components/Base'
+
 function App() {
-	return <div>Login page.</div>
+	return <div>Not Found</div>
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<Base>
+			<App />
+		</Base>
 	</React.StrictMode>
 )
